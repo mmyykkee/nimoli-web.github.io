0f3ad86975b2fc103fbb6725c72e072cf4bd1ba1
mmyykkee <83122061+mmyykkee@users.noreply.github.com>
2022-03-03 09:39:30 +0300

    Side bar menu

--- nimoli.js
+++ nimoli.js
@@ -12,7 +12,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 5000,
+    delay: 500,
     disableOnInteraction: false,
   },
   pagination: {
