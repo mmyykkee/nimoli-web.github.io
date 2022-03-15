6270d2f351c1028dd92700b73330a703ffd96faf
mmyykkee <83122061+mmyykkee@users.noreply.github.com>
2022-03-15 15:39:10 +0300

    all @media fixes

--- nimoli.js
+++ nimoli.js
@@ -29,7 +29,7 @@ var Swiper = new Swiper(".mySwiper", {
   spaceBetween: 2,
   centeredSlides: true,
   autoplay: {
-    delay: 5000,
+    delay: 500000,
     disableOnInteraction: false,
   },
   pagination: {
